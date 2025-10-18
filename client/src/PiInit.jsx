d79cb8e7a4e996eb090f57a8426ada84e557e0ad
ninja99524 <konradmhau@gmail.com>
2025-10-18 15:27:44 +0545

    Update PiInit.jsx for clarity in comments

--- client/src/PiInit.jsx
+++ client/src/PiInit.jsx
@@ -5,7 +5,8 @@ function PiInit() {
     if (window.Pi) {
       window.Pi.init({
         version: "2.0",
-        sandbox: true // IMPORTANT: enables Pi Testnet sandbox
+        sandbox: true, // enables Pi Testnet
+        appId: process.env.REACT_APP_PI_API_KEY // your Pi API key from Vercel env
       });
     }
   }, []);
